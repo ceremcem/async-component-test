a8e9814d7ff2cf8d46bd4313f55b652ce3da62ff
Cerem Cem ASLAN <ceremcem@ceremcem.net>
2017-10-23 23:10:54 +0300

    Create component.js

---
+++ component.js
@@ -0,0 +1,16 @@
+// Define your component 
+myComponent = {
+	template: 'I have loaded asynchronously <button on-click="foo">foo!</button>',
+	on: {
+		foo: function(){
+			alert('this is foo button!');
+		}
+	}
+}
+
+// 1. Serialize the component 
+console.log("serialized component: ");
+console.log(serialize(myComponent)); 
+
+// Copy the output to the receiver
+
