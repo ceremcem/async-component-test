e6de1a945fcd704e5b2ddf0fdff5c15b3dc1dd51
Cerem Cem ASLAN <cem@aktos.io>
2017-10-23 22:52:05 +0300

    initial commit

---
+++ lib.js
@@ -0,0 +1,28 @@
+// JSONfn: Taken from https://stackoverflow.com/a/17839858/1952991
+var JSONfn;
+if (!JSONfn) {
+    JSONfn = {};
+}
+
+(function () {
+  JSONfn.stringify = function(obj) {
+    return JSON.stringify(obj,function(key, value){
+            return (typeof value === 'function' ) ? value.toString() : value;
+        });
+  }
+
+  JSONfn.parse = function(str) {
+    return JSON.parse(str,function(key, value){
+        if(typeof value != 'string') return value;
+        return ( value.substring(0,8) == 'function') ? eval('('+value+')') : value;
+    });
+  }
+}());
+
+serialize = function(x){
+    return btoa(JSONfn.stringify(x))
+}
+
+deserialize = function(x){
+    return Ractive.extend(JSONfn.parse(atob(x)))
+}
