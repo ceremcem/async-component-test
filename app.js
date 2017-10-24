5b5707960c505c5adac5e1fdda284359ff3201a1
Cerem Cem ASLAN <ceremcem@ceremcem.net>
2017-10-24 22:06:02 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
 httpGet('app.html', function(app_html){
-  httpLoad('serialize.js', function(res){
-    console.log('this is received from serialize.js: ', res);
-  });
   Ractive.components.waitForIt = function(){
     return new Promise(ok => {
       httpGet('myComponent.serialized', function(received){
