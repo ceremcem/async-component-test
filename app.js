c3db9d27ba710d37efe2c0e4a0e7aa7a303f7979
Cerem Cem ASLAN <ceremcem@ceremcem.net>
2017-10-24 21:38:04 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-Ractive.components.waitForIt = function(){
-  return new Promise(ok => {
-    setTimeout(() => {
-      received = "eyJ0ZW1wbGF0ZSI6IkkgaGF2ZSBsb2FkZWQgYXN5bmNocm9ub3VzbHkgPGJ1dHRvbiBvbi1jbGljaz1cImZvb1wiPmZvbyE8L2J1dHRvbj4iLCJvbiI6eyJmb28iOiJmdW5jdGlvbiAoKXtcblx0XHRcdGFsZXJ0KCd0aGlzIGlzIGZvbyBidXR0b24hJyk7XG5cdFx0fSJ9fQ"
-      ok(Ractive.extend(deserialize(received)));
-    }, 1000);
-  });
-}
-
-httpGet('app.html', function(template){
-    new Ractive({
-      target: 'body',
-      template: template
+httpGet('app.html', function(app_html){
+  Ractive.components.waitForIt = function(){
+    return new Promise(ok => {
+      httpGet('myComponent.serialized', function(received){
+        ok(Ractive.extend(deserialize(received)));
+      })
     });
+  }
+
+  new Ractive({
+    target: 'body',
+    template: app_html
+  });
 })
