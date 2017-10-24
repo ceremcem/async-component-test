3456be94f116ebbd19be8d0088a3c3a98786893e
Cerem Cem ASLAN <ceremcem@ceremcem.net>
2017-10-25 00:07:02 +0300

    Delete serialize.js

--- serialize.js
+++
@@ -1,6 +0,0 @@
-httpGet('myComponent.js', function(received){
-  eval(received);
-  console.log("serialized component: ");
-  console.log(myComponentSerialized); 
-  document.getElementById("output").innerHTML = myComponentSerialized;
-});
