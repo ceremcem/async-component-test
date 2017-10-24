be141b7095f9ef41ccc0e77fdfc596da17a19ab6
Cerem Cem ASLAN <ceremcem@ceremcem.net>
2017-10-25 00:00:25 +0300

    Update myComponent.js

--- myComponent.js
+++ myComponent.js
@@ -1,6 +1,6 @@
 // Define your component without `Ractive.extend`
 myComponent = {
-  template: '<div>I have loaded asynchronously <button on-click="foo">foo!</button></div>',
+  template: '<div style="border: 1px solid black">I have loaded asynchronously <button on-click="foo">foo!</button></div>',
   on: {
     foo: function(){
       alert('this is foo button!');
@@ -8,4 +8,5 @@ myComponent = {
   }
 }
 
+// fully serialize the component in order to send over a websocket transport
 myComponentSerialized = serialize(myComponent);
