6dc0e7e524168ec86467ad2d9a21ee09ba3feaa3
Cerem Cem ASLAN <ceremcem@ceremcem.net>
2017-10-24 23:57:38 +0300

    Update serialize.js

--- serialize.js
+++ serialize.js
@@ -1,7 +1,6 @@
 httpGet('myComponent.js', function(received){
   eval(received);
-  // 1. Serialize the component 
   console.log("serialized component: ");
-  console.log(serialize(myComponent)); 
-  document.getElementById("output").innerHTML = serialize(myComponent);
+  console.log(myComponentSerialized); 
+  document.getElementById("output").innerHTML = myComponentSerialized;
 });
