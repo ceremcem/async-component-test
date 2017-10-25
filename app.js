75f5424b2564737603b380daae84f6a97e2269e1
Cerem Cem ASLAN <ceremcem@ceremcem.net>
2017-10-25 11:14:51 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ Ractive.components.waitForIt = function(){
   return new Promise(ok => {
     httpGet('myComponent.js', function(received){
       eval(received);
+      // you can also use serialized version by using `serialize(myComponent)' 
+      // and then `deserialize()` it. 
       ok(Ractive.extend(myComponent));
     })
   });
